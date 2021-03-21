7148fd0608927f189a9fe5f3100e7aef24c230ce
Andrew <vozniukandrey@gmail.com>
2021-03-21 16:49:33 +0200

    add 404 page

---
+++ src/shared/404/404.js
@@ -0,0 +1,11 @@
+import styles from './404.module.scss'
+
+function page404() {
+  return <div className={styles.wrapper}>
+    <div className={styles.content}>
+      Page not found, sorry 😞
+    </div>
+  </div>
+}
+
+export default page404
