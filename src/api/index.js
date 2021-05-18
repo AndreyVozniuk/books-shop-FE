6cc673e87cfc059570533a8615535cd5a5f14921
Andrew_vdp <andriy.v.dp@gmail.com>
2021-05-18 11:49:31 +0300

    change searchBooks route on filterBooks

--- src/api/index.js
+++ src/api/index.js
@@ -2,5 +2,5 @@ import { get } from './axiosHelper'
 
 export const loadBooks = () => get('/books')
 export const searchBooks = (searchTxt, option) => get(`/searchBooks?value=${searchTxt}&option=${option}`)
-export const filterBooks = (categories, price) => get(`/searchBooks?categories=${categories}&price=${price}`)
+export const filterBooks = (categories, price) => get(`/filterBooks?categories=${categories}&price=${price}`)
 
